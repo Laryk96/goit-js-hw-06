6ba0792dede811ba1a8c7972fdb015f5f8668d3e
Lary <110429721+Laryk96@users.noreply.github.com>
2022-11-06 13:39:06 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,7 +3,6 @@ const outputEl = document.querySelector("#name-output");
 
 inputEl.addEventListener("input", () => {
   const userName = inputEl.value.trim();
-  console.log(userName);
-  console.log(inputEl.value);
+
   outputEl.textContent = userName !== "" ? userName : "Anonymous";
 });
